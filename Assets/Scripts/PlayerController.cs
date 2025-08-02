68961a2f574b11dd0a629eee947d7fd47c7b839f
ZaharyN <124094259+ZaharyN@users.noreply.github.com>
2025-08-02 20:24:18 +0300

    Input Action added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,38 +3,24 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Input Actions")]
+    [SerializeField] private InputAction moveAction;
+
+    [Header("Character speed")]
     [SerializeField] private float horizontalSpeed = default;
     [SerializeField] private float verticalSpeed = default;
 
-    // Update is called once per frame
-    void Update()
+	private void Start()
+	{
+        moveAction.Enable();
+	}
+
+	// Update is called once per frame
+	void Update()
     {
-        float horizontal = 0.0f;
-        float vertical = 0.0f;
+        Vector2 move = moveAction.ReadValue<Vector2>();
 
-        if (Keyboard.current.leftArrowKey.isPressed || Keyboard.current.aKey.isPressed)
-        {
-            horizontal = -1.0f;
-        }
-        else if (Keyboard.current.rightArrowKey.isPressed || Keyboard.current.dKey.isPressed)
-        {
-            horizontal = 1.0f; 
-        }
-
-        if (Keyboard.current.upArrowKey.isPressed || Keyboard.current.wKey.isPressed)
-        {
-            vertical = 1.0f;
-        }
-        else if (Keyboard.current.downArrowKey.isPressed || Keyboard.current.sKey.isPressed)
-        {
-            vertical = -1.0f;   
-        }
-
-        Vector2 position = transform.position;
-
-        position.x += 0.1f * horizontal;
-        position.y += 0.1f * vertical;
-
+        Vector2 position = (Vector2)transform.position + move*0.1f;
         transform.position = position;
     }
 }
