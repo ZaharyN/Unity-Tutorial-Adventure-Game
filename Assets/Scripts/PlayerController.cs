169150b9440c4234bc490ac6cc137474414473a3
ZaharyN <124094259+ZaharyN@users.noreply.github.com>
2025-08-02 22:03:32 +0300

    Applied slicing to the tilesets and made movement framerate independant

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,24 +3,22 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("Input Actions")]
-    [SerializeField] private InputAction moveAction;
+	[Header("Input Actions")]
+	[SerializeField] private InputAction moveAction;
 
-    [Header("Character speed")]
-    [SerializeField] private float horizontalSpeed = default;
-    [SerializeField] private float verticalSpeed = default;
+	[Header("Character speed")]
+	[SerializeField] private float movementSpeed = 3f;
 
 	private void Start()
 	{
-        moveAction.Enable();
+		moveAction.Enable();
 	}
 
-	// Update is called once per frame
 	void Update()
-    {
-        Vector2 move = moveAction.ReadValue<Vector2>();
+	{
+		Vector2 move = moveAction.ReadValue<Vector2>();
 
-        Vector2 position = (Vector2)transform.position + move*0.1f;
-        transform.position = position;
-    }
+		Vector2 position = (Vector2)transform.position + Time.deltaTime * move * movementSpeed;
+		transform.position = position;
+	}
 }
